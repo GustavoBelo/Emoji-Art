4080003bfa4c03eb63ed94764d30e197afc47232
Gustavo Belo <gustavobelo28@gmail.com>
2021-03-23 10:07:43 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Gustavo Belo on 23/03/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
