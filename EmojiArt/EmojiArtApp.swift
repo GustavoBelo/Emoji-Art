3a1ada56779edae8b404853b491c094ddf6b07c7
Gustavo Belo <gustavobelo28@gmail.com>
2021-04-05 16:54:02 -0300

    Until Lecture 13 - I changed the save from userDefaults to FileManager

--- EmojiArt/EmojiArtApp.swift
+++ EmojiArt/EmojiArtApp.swift
@@ -10,7 +10,8 @@ import SwiftUI
 @main
 struct EmojiArtApp: App {
     var body: some Scene {
-        let store = EmojiArtDocumentStore(named: "Emoji Art")
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let store = EmojiArtDocumentStore(directory: url)
         WindowGroup {
 //            EmojiArtDocumentView(document: EmojiArtDocument())
             EmojiArtDocumentChooser().environmentObject(store)
